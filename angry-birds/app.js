1acb309c8b47a92327ea2d89ffbe80d3561c76ed
Quentin Viergever <9016605@student.tcrmbo.nl>
2022-05-20 14:11:52 +0200

    "oefening 1 - Move the bird"

--- angry-birds/app.js
+++ angry-birds/app.js
@@ -1,9 +1,32 @@
 //Oefening angry-birds
 
-const bird = document.querySelector(`.bird`);
-let position = 0;
+const bird = document.querySelector(".bird");
+let px = 0;
 
 bird.addEventListener("click", function(){
-    position = position + 50;
-    bird.style.left = position + 'px';
+    px = px + 50;
+    bird.style.left = px + "px";
+});                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+
+window.addEventListener("keydown", function(e){
+    console.log(e.key);
+    if(e.key == "ArrowRight"){
+        px = px + 50;
+        bird.style.left = px + "px";
+    }
+
+    if(e.key == "ArrowLeft"){
+        px = px - 50;
+        bird.style.left = px + "px";
+    }
+
+    if(e.key == "ArrowUp"){
+        px = px + 50;
+        bird.style.bottom = px + "px";
+    }
+
+    if(e.key == "ArrowDown"){
+        px = px - 50;
+        bird.style.bottom = px + "px";
+    }       
 });
